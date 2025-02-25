df2c9148fefbc52260c8246cfc61e7c32400100d
Eliza Frankel <elizaafrankel@gmail.com>
2025-02-25 11:14:01 -0700

    Updating RA_cap_bound() to take hourangles or degrees & updating spherical_cap_output() to be more universally usable

--- week5/spherical_caps_w5.py
+++ week5/spherical_caps_w5.py
@@ -1,11 +1,11 @@
 #!/usr/local/Anaconda2024/bin/python3.11
 
 #######################################
-#						              #
-#    In Class Work: Spherical Caps    #
-#		       2.20.2025			  #
-#	      Author: Eliza Frankel       #
-#                                     #                        
+#						              
+#    In Class Work: Spherical Caps    
+#		       2.20.2025			  
+#	      Author: Eliza Frankel       
+#                                                             
 #######################################
 
 import matplotlib.pyplot as plt
@@ -20,7 +20,7 @@ def RA_cap_bound(RA):
     """ Converts RA from hms to cartesian coordinates
     
     Parameters:
-    ra (string): the right ascension, specifically in units of hour angles
+    ra (string): the right ascension, specifically in units of hour angles or degrees
 
     Returns:
     np array consisting of:
@@ -29,10 +29,17 @@ def RA_cap_bound(RA):
 	    z (float or int): z component of RA
 	    l_cos0 (float or int): Width of RA component (1-h)
     """
+
     declination='0d0m0s'
 
+    # EAF - Determining whether RA is in hourangles or degrees
+    if 'h' in RA:
+    	coord = SkyCoord(RA, declination, frame='icrs', unit=(u.hourangle, u.degree))
+    else:
+    	coord = SkyCoord(RA, declination, frame='icrs', unit=(u.degree, u.degree))
+    
+
 	# EAF - I can't think of a better way to add the 90 degrees to the RA than this 
-    coord = SkyCoord(RA, declination, frame='icrs', unit=(u.hourangle, u.degree))
     coord_degrees = SkyCoord(ra = coord.ra.degree + 90, dec = coord.dec.degree, unit=(u.degree, u.degree))
     coord_degrees.representation_type = 'cartesian'
 
@@ -80,7 +87,7 @@ def spherical_cap(coordinates, theta):
 
 	Parameters:
 	coordinates (SkyCoord object): the coordinates of the ra and dec
-	theta (float or int)
+	theta (float or int): should be in degrees
 
 	Returns:
 	np.array consisting of:
@@ -103,28 +110,36 @@ def spherical_cap(coordinates, theta):
 ###  TASK 4  ###
 
 # EAF - the following is a function to write the results of the above 3 functions
-#		to a text file. I was unsure if I should hardcode the amount of lines, or
-#		if I should do a for loop. In this case, I opted for hardcoding it in.
+#		to a text file. 
+# 		It has been modified from the previous submission to allow for differing numbers
+#		of polygons and caps to be used.
 
-def spherical_cap_output(caps):
+def spherical_cap_output(caps, polygons, outfile_name):
 	""" Writes output of RA_cap_bound(), Dec_cap_bound(), and spherical_cap() to a file
 
 	Parameters:
-	caps (np.array): list of arrays from the ra, dec, and spherical cap functions
+	caps (list or np.array): list of arrays from the ra, dec, and spherical cap functions
+	polygons (int): the number of polygons in the output file
+	outfile_name (string): a string containing the name you'd like to save your file as
+		- Should also include the suffix (ie .txt, .ply, etc.)
 
 	Notes:
 		Creates a text file with the desired results
+		* number of caps must be divisible by number of polygons in this iteration of the function
+			- Currently thinking of something better to do this.
 	"""
-	ra = caps[0]
-	dec = caps[1]
-	spherical = caps[2]
 
-	with open('cap_output.txt', 'w') as file:
-		file.write('polygon 1 ({} caps, 1 weight, 0 pixel, 0 str):\n'.format(len(caps)))
-		file.write('\t{} {} {} {}\n'.format(ra[0], ra[1], ra[2], ra[3]))
-		file.write('\t{} {} {} {}\n'.format(dec[0], dec[1], dec[2], dec[3]))
-		file.write('\t{} {} {} {}\n'.format(spherical[0], spherical[1], spherical[2], spherical[3]))
-
+	with open(outfile_name, 'w') as file:
+		index = 0
+		file.write(f'{polygons} polygons\n')
+		# EAF - creates the number of polygons in the file
+		for i in range(polygons):
+			file.write(f'polygon {i+1} ( {len(caps) // polygons} caps, 1 weight, 0 pixel, 0 str):\n')	
+			
+			# EAF - For each cap, write the data in this specified format
+			for j in range(len(caps) // polygons):
+				file.write(f'\t{caps[index][0]:19.16f} {caps[index][1]:19.16f} {caps[index][2]:19.16f} {caps[index][3]:19.16f}\n')
+				index += 1
 
 
 if __name__ == "__main__":
@@ -143,4 +158,4 @@ if __name__ == "__main__":
 
 	# Task 4
 	caps = np.array([RA, Dec, sphere])
-	spherical_cap_output(caps)
+	spherical_cap_output(caps, 1, 'cap_output.txt')
